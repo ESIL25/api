2614e5016be15aea2180ed96292dcccc4960d0f5
ALPHAKNOW <166359862+ESIL25@users.noreply.github.com>
2024-04-08 21:01:01 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# 앱 제목 설정
+st.title('지도 API 실습')
+
+# HTML 및 자바스크립트를 포함한 지도를 표시
+components.html(
+    """
+    <html>
+    <head>
+    <title>vworld 지도 예제</title>
+    <script type="text/javascript" src="https://map.vworld.kr/js/vworldMapInit.js.do?version=2.0&apiKey=여러분의_API_키"></script>
+    </head>
+    <body>
+    <div id="vworldMap" style="width:100%;height:80vh;"></div>
+    <script type="text/javascript">
+    var map;
+    function init() {
+        var vw = new VWorldMap('vworldMap');
+        vw.setMapMode('2d');
+        map = vw.getMap();
+    }
+    window.onload = init;
+    </script>
+    </body>
+    </html>
+    """,
+    height=800
+)
